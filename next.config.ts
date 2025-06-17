bd3e8b76a0ba8f0df4a9f5b96317edd6b62fde38
HoanIT <lehoan.dev@gmail.com>
2025-06-17 18:33:55 +0700

    chore: ignore during build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import { env } from "env";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: env.NODE_ENV === 'production',
+  },
 };
 
 export default nextConfig;
